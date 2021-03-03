82e40fa3c8a2aadb639d4d7f6144467681cae19a
Marcus <46597619+Marcus4420@users.noreply.github.com>
2021-03-03 19:13:30 +0100

    Recieves messages server side + added MONK for MongoDB

--- server/index.js
+++ server/index.js
@@ -12,8 +12,25 @@ app.get('/', (req, res) => {
     })
 })
 
+function isValidMsg(msg) {
+    return msg.name && msg.name.toString().trim() != '' && msg.content && msg.content .toString().trim() != '' 
+}
+
 app.post('/messages', (req, res) => {
-    console.log(req.body)
+    if (isValidMsg(req.body)) {
+    //Gør noget
+    const msg = {
+        name: req.body.name.toString(),
+        content: req.body.content.toString() 
+    }
+    console.log(msg);
+    } 
+    else {
+    res.status(422)
+    res.json({
+        message: 'Navn og indhold er nødvendigt'
+    });
+}
 });
 
 app.listen(5000, () => {
